914611b7c9cc3c72d962c7be6d540821938718fc
Alec Springel <alecspringel@gmail.com>
2022-12-08 15:09:18 -0800

    updated gatsby-config for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `SEO Title`,
-    description: `SEO Description`,
-    url: "https://www.blueprint.com",
-    image: "/image-within-the-static-folder.jpg",
-    author: `@alecspringel`,
+    title: 'Sungrown Farms',
+    description: 'Land leases and licensing for cannabis farms in Washington state',
+    url: 'https://www.sungrownfarms.net',
+    image: '/sungrown-drone.jpg',
+    author: '@alecspringel',
   },
   plugins: [
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-s3`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        bucketName: "alecspringel.com",
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Site Name`,
-        short_name: `Short Site Name`,
-        start_url: `/`,
-        background_color: `#343434`,
-        theme_color: `#343434`,
-        display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        name: 'Sungrown Farms',
+        short_name: 'Sungrown Farms',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#fff',
+        display: 'browser',
+        icon: 'src/images/manifest/32x32.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-plugin-s3',
+      options: {
+        bucketName: process.env.DEPLOY_ENV === 'prod' ? 'sungrownfarms.com' : 'sungrownfarms.com-dev',
+      },
+    },
   ],
 };
